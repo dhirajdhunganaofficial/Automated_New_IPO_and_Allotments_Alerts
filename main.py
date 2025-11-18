c8c5f2d48f519342c16e81b74971bad3770f7a3f
Dhiraj Dhungana <dhirajdhunganaofficial@gmail.com>
2025-11-18 20:47:45 +1100

    Added codes to check the IPO issues and total number of applied shares.

--- main.py
+++ main.py
@@ -39,4 +39,47 @@ password.send_keys("Deerw@lk25")
 login = driver.find_element(By.CLASS_NAME, "sign-in")
 login.click()
 
-sleep(15)
+menu = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".mdi-menu"))
+)
+menu.click()
+
+myASBA = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".msi-asba"))
+)
+myASBA.click()
+
+# Wait until ASBA page loads
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "app-asba ul li")))
+
+applyForIssue = driver.find_element(By.CSS_SELECTOR, "app-asba ul li:nth-child(1)")
+applyForIssue.click()
+
+ipo_items = driver.find_elements(By.CSS_SELECTOR, "app-share-list .card")
+
+if len(ipo_items) == 0:
+    print("No IPO listings available.")
+else:
+    print(f"Found {len(ipo_items)} IPO(s).")
+
+applicationReport = driver.find_element(By.CSS_SELECTOR, "app-asba ul li:nth-child(3)")
+applicationReport.click()
+
+
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/div/app-asba/div/div[2]/app-share-list/div/div/div[2]/div[1]')))
+parentDiv = driver.find_element(By.XPATH, '//*[@id="main"]/div/app-asba/div/div[2]/app-share-list/div/div/div[2]/div[1]')
+childDivs = parentDiv.find_elements(By.XPATH, "./div")
+
+# Safely get applied shares
+appliedShares = len(childDivs)
+
+if len(childDivs) == 0:
+    print("No applied shares found.")
+else:
+    print(f"Found {len(childDivs)} applied shares.")
+
+
+
+sleep(10)
+driver.close()
+driver.quit()
