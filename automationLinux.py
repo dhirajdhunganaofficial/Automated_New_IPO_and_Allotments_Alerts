76f80b1c361601b3fc71cc38fa8323eb32ec3c02
Dhiraj Dhungana <dhirajdhunganaofficial@gmail.com>
2026-01-06 04:42:48 +1100

    For debug

--- automationLinux.py
+++ automationLinux.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def runAutomation(dp, username, password):
+def runAutomation(username, password):
     # Chrome options for headless Linux
     chrome_options = Options()
     chrome_options.add_argument('--headless')
