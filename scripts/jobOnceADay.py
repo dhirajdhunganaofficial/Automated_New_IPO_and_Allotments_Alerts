5ab2878bbee4a3866376cb338638e6129dc1a4c5
Dhiraj Dhungana <dhirajdhunganaofficial@gmail.com>
2026-01-13 02:27:06 +1100

    for absolute path for the job

--- scripts/jobOnceADay.py
+++ scripts/jobOnceADay.py
@@ -1,3 +1,14 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
 from flask import Flask
 from configLinux import Config
 from models import Subscriber
